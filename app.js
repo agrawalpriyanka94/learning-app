bea7620784702b80902583971b8787340da2e42c
Mahendra Prajapati <pra_mahendra@hotmail.com>
2021-03-02 22:15:18 +0530

    2. post mthod added without business logic

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ const express = require('express');
 // creating object of Express named as app
 const app = express();
 
+// adding middleware for accessing request body in json format
+app.use(express.json());
+
+// adding post method
+app.post('/api/v1/user', (request, response) => {
+    console.log(request.body);
+
+    const result = request.body;
+
+    response.status(201).send(result);
+});
+
 // Starting the server at some port
 const port = 8989;
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server started at ${port}.`);
 });
