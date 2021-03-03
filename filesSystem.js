a43bdecd2e2dcfb12e0eecf61acb0ec5485bc604
agrawalpriyanka94 <agrawalpriyanka94@gmail.com>
2021-03-03 09:52:34 -0800

    4.1 write method updated for db

--- filesSystem.js
+++ filesSystem.js
@@ -7,7 +7,7 @@ fs.readFile('./database/object.json', 'UTF-8', (err, data) => {
 
 exports.writeDatabase = async (data) => {
     try {
-        await fs.writeFile('./database/object.json', data, err => console.log(err));
+        await fs.writeFile('./database/object.json', data.stringify(data, null, 2), err => console.log(err));
     } catch (err) {
         console.log(err);
     }
